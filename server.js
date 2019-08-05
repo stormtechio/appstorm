e66efc1db0bdf419aa023ec42144a39310af678f
Saori <saoricosta@gmail.com>
2019-08-05 10:06:12 -0300

    Criação de servidor

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const http = require('http');
+const debug = require('debug')('appstorm:server');
+const express = require('express');
+
+const app = express();
+const port = 5000;
+app.set('port', port);
+
+const server = http.createServer(app);
+const router = express.Router();
+
+
+server.listen(port);
+console.log('Rodando na porta: ' + port);
